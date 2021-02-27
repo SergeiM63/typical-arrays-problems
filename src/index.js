bf7997ebd62f78fe1b7e6837ac7edc4996059c4d
Sergei M <61053189+SergeiM63@users.noreply.github.com>
2021-02-27 13:30:01 +0400

    fix: fix code

---
+++ src/index.js
@@ -0,0 +1,19 @@
+
+exports.min = function min (array) {
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  return Math.min(...array);
+}
+
+exports.max = function max (array) {
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  return Math.max(...array);
+
+}
+
+exports.avg = function avg (array) {
+  if(array === undefined) return 0;
+  if(array.length === 0) return 0;
+  return array.reduce((a, b) => (a + b)) / array.length;
+}
